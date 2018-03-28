43dacd4b53c1805ade86b8552e9a3f7647b62b16
UO266190 <UO266190@DESKTOP-9C520EC>
2018-03-28 22:51:06 +0200

    controllers / routes

--- Multichat/Multichat/app_server/controllers/controllers.js
+++ Multichat/Multichat/app_server/controllers/controllers.js
@@ -1,3 +1,27 @@
-﻿module.exports.index = function (req, res, next) {
+﻿module.exports.about = function (req, res, next) {
+    res.render('about', {});
+};
+
+module.exports.index = function (req, res, next) {
     res.render('index', {});
+};
+
+module.exports.login = function (req, res, next) {
+    res.render('login', {});
+};
+
+module.exports.logout = function (req, res, next) {
+    res.render('logout', {});
+};
+
+module.exports.profile = function (req, res, next) {
+    res.render('profile', {});
+};
+
+module.exports.register = function (req, res, next) {
+    res.render('register', {});
+};
+
+module.exports.multichat = function (req, res, next) {
+    res.render('multichat', {});
 };
--- Multichat/Multichat/app_server/routes/routes.js
+++ Multichat/Multichat/app_server/routes/routes.js
@@ -3,5 +3,12 @@ var router = express.Router();
 var ctrlMain = require('../controllers/controllers');
 
 router.get('/', ctrlMain.index);
+router.get('/about', ctrlMain.about);
+router.get('/index', ctrlMain.index);
+router.get('/login', ctrlMain.login);
+router.get('/logout', ctrlMain.logout);
+router.get('/profile', ctrlMain.profile);
+router.get('/register', ctrlMain.register);
+router.get('/multichat', ctrlMain.multichat);
 
 module.exports = router;
