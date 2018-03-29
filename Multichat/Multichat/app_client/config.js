8481c0e6fcaae9ef08dec703de5b01312ae639ae
UO266190 <UO266190@DESKTOP-9C520EC>
2018-03-29 21:16:31 +0200

    ya se ven las imagenes

---
+++ Multichat/Multichat/app_client/config.js
@@ -0,0 +1,3 @@
+﻿angular.module('multichatApp', []);
+
+var config = {};
--- Multichat/Multichat/app_server/config.js
+++ Multichat/Multichat/app_server/config.js
@@ -1,6 +1,4 @@
-﻿//angular.module('multichatApp', []);
-
-var config = {};
+﻿var config = {};
 //Heroku assigns the value through the PORT environment variable. You cannot choose
 //config.port = process.env.HTTP_PORT || process.env.PORT || 3000;
 config.port = 3000;
