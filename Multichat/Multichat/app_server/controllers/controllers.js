dd3d82947beca69d25d4e1b2e34c69d400a328aa
UO266190 <UO266190@DESKTOP-9C520EC>
2018-04-02 18:37:11 +0200

    controllers

--- Multichat/Multichat/app_server/controllers/controllers.js
+++ Multichat/Multichat/app_server/controllers/controllers.js
@@ -31,48 +31,54 @@ module.exports.about = function (req, res, next) {
 };
 
 module.exports.index = function (req, res, next) {
-    res.render('index', {
-        lang: {
-            description: 'MultiChat with lots of features: texts, icons, pictures, '
-                + 'voice, files, videoconference, radio and video broadcasting, collaborative '
-                + 'drawing and presentations!',
-            message: 'MultiChat',
-            title: 'MultiChat: Home',
-            feature1: {
-                name: 'Messages',
-                description1: 'Send and receive texts and icons',
-                description2: 'Even pictures and attached files'
-            },
-            feature2: {
-                name: 'Broadcasts',
-                description1: 'Radio and video broadcasting',
-                description2: 'Enjoy with your friends'
-            },
-            feature3: {
-                name: 'Videoconferences',
-                description1: 'Video and audio sharing',
-                description2: 'Very scalable implementation'
-            },
-            feature4: {
-                name: 'Drawings',
-                description1: 'Collaborative drawing',
-                description2: 'Different types of elements'
-            },
-            feature5: {
-                name: 'Presentations',
-                description1: 'Collaborative presentations',
-                description2: 'See presentations online'
-            },
-            feature6: {
-                name: 'Geolocalition',
-                description1: 'Geolocation of users',
-                description2: 'See where your friends are'
-            },
-            info: 'More info',
-            login: 'Login',
-            register: 'Register'
-        }
-    });
+    if (req.sub === false) {
+        res.render('index', {
+            lang: {
+                description: 'MultiChat with lots of features: texts, icons, pictures, '
+                    + 'voice, files, videoconference, radio and video broadcasting, collaborative '
+                    + 'drawing and presentations!',
+                message: 'MultiChat',
+                title: 'MultiChat: Home',
+                feature1: {
+                    name: 'Messages',
+                    description1: 'Send and receive texts and icons',
+                    description2: 'Even pictures and attached files'
+                },
+                feature2: {
+                    name: 'Broadcasts',
+                    description1: 'Radio and video broadcasting',
+                    description2: 'Enjoy with your friends'
+                },
+                feature3: {
+                    name: 'Videoconferences',
+                    description1: 'Video and audio sharing',
+                    description2: 'Very scalable implementation'
+                },
+                feature4: {
+                    name: 'Drawings',
+                    description1: 'Collaborative drawing',
+                    description2: 'Different types of elements'
+                },
+                feature5: {
+                    name: 'Presentations',
+                    description1: 'Collaborative presentations',
+                    description2: 'See presentations online'
+                },
+                feature6: {
+                    name: 'Geolocalition',
+                    description1: 'Geolocation of users',
+                    description2: 'See where your friends are'
+                },
+                info: 'More info',
+                login: 'Login',
+                register: 'Register'
+            }
+        });
+    }
+    else
+    {
+        module.exports.multichat(req, res, next);
+    }
 };
 
 module.exports.login = function (req, res, next) {
