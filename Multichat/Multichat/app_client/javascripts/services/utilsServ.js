c500a4be8fd8583dd80ce343993fcfa42b3e3a04
UO266190 <UO266190@DESKTOP-9C520EC>
2018-04-02 23:39:22 +0200

    servicios

---
+++ Multichat/Multichat/app_client/javascripts/services/utilsServ.js
@@ -0,0 +1,19 @@
+﻿angular.module('multichatApp').service('utils', function () {
+    function isJson(str) {
+        try
+        {
+            JSON.parse(str);
+        }
+        catch (e)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    var methods = {
+        isJson: isJson
+    };
+
+    return methods;
+});
---
+++ Multichat/Multichat/app_client/javascripts/services/webSocketManagerServ.js
@@ -0,0 +1,34 @@
+﻿angular.module('multichatApp').service('webSocketManager', function ($websocket, growl, utils) {
+    if (!window.WebSocket)
+    {
+        console.log("WebSockets NOT supported.");
+        alert("Consider updating your browser for a better experience.");
+    }
+
+    var HOST = location.origin.replace(/^http/, 'ws');
+    var ws = $websocket(HOST);
+    ws.onOpen(function () {
+        console.log("Open");
+        growl.success('Server started. Enjoy!', { title: 'Success', });
+        setInterval(function () {
+            ws.send('ping at ' + new Date().getUTCSeconds());
+        }, 30000);
+    });
+
+    window.onbeforeunload = function () {
+        ws.close();
+    };
+
+    ws.onMessage(function (message) {
+        if (utils.isJson(message.data))
+        {
+            var obj = JSON.parse(message.data);
+        }
+    });
+
+    var methods = {
+        ws: ws,
+    };
+
+    return methods;
+});
