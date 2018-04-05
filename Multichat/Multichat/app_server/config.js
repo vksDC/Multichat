faf5fcd9b2d57d0defbf1cea1dc6bbfe730f45ec
UO266190 <UO266190@DESKTOP-9C520EC>
2018-04-05 21:53:38 +0200

    emisora de radio

--- Multichat/Multichat/app_server/config.js
+++ Multichat/Multichat/app_server/config.js
@@ -3,6 +3,6 @@
 //config.port = process.env.HTTP_PORT || process.env.PORT || 3000;
 config.port = 3000;
 
-config.audioUrl = 'http://ruhit.imgradio.pro:80/RusHit48'; //shoutcast (in .pls file)
+config.audioUrl = 'http://156.35.98.53:8001/Multichat'; //shoutcast (in .pls file)
 
 module.exports = config;
